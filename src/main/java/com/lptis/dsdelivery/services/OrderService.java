ebfad2a56420046a1817adbaabd55b4367d53d10
Felipe Neves <felipeneves@sistemafieg.org.br>
2021-01-20 17:32:37 -0300

    Criação do endpoint POST de order

--- src/main/java/com/lptis/dsdelivery/services/OrderService.java
+++ src/main/java/com/lptis/dsdelivery/services/OrderService.java
@@ -3,6 +3,7 @@ package com.lptis.dsdelivery.services;
 import com.lptis.dsdelivery.dto.OrderDTO;
 import com.lptis.dsdelivery.dto.ProductDTO;
 import com.lptis.dsdelivery.entities.Order;
+import com.lptis.dsdelivery.entities.OrderStatus;
 import com.lptis.dsdelivery.entities.Product;
 import com.lptis.dsdelivery.repositories.OrderRepository;
 import com.lptis.dsdelivery.repositories.ProductRepository;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,10 +21,25 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll(){
         List<Order> list = orderRepository.findPendingOrdersWithProducts();
         return list.stream().map(order -> new OrderDTO((order))).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto){
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        for(ProductDTO p : dto.getProducts()){
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = orderRepository.save(order);
+        return new OrderDTO(order);
     }
 
 }
